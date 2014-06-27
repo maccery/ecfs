cd5df29099c8c5f610233b7746f83d64b36c2831
Tom Macmichael <tommacmichael@me.com>
2014-06-27 17:51:08 +0100

    Moved logo, added info link

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,8 +4,8 @@
 
         public function index()
         {
-            $data['title'] = 'ECFS 2015';
-            $data['description'] = 'ECFS 2015 - coming soon.';
+            $data['title'] = 'ECFS 2015 - Edinburgh Charity Fashion Show';
+            $data['description'] = 'Edinburgh Charity Fashion Show (ECFS) 2015 - coming soon.';
 
             $this->load->view('common/header', $data);
             $this->load->view('home');
--- application/views/common/header.php
+++ application/views/common/header.php
@@ -9,7 +9,6 @@
     <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
     <script src="//d2hw98731ell8t.cloudfront.net/assets/js/jquery.min.js" type="text/javascript"></script>
     <script src="//maps.google.com/maps/api/js?libraries=geometry,places&sensor=false" type="text/javascript"></script>
-    <script type="text/javascript" src="//www.google.com/recaptcha/api/js/recaptcha_ajax.js"></script>
     <meta name="copyright" content="Copyright Maccery.com 2014">
     <meta name="author" content="Maccery.com">
     <meta name="email" content="t.macmichael@edinburghcharityfashionshow.org">
@@ -17,9 +16,10 @@
     <meta name="Revisit-after" content="1 Days">
     <meta name="ROBOTS" content="INDEX, FOLLOW">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta property="og:image" content="/assets/images/fb-share-logo.png">
+    <meta property="og:image" content="/assets/images/ecfs-logo.png">
     <meta property="og:title" content="<?= $title ?>">
     <meta property="og:description" content="<?= $description ?>">
+    <link rel='shortcut icon' type='image/x-icon' href="/assets/img/favicon.ico">
     <meta name="description" content="<?= $description ?>" />
 
     <title><?= $title ?></title>
@@ -31,8 +31,10 @@
     </video>
     <div class="container">
         <div class="row text-center">
+            <p><img  width="190" src="/assets/img/ecfs-logo.png"></p>
             <h1>ECFS 2015</h1>
-            <p>It's coming.</p>
-            <p><a href="http://2013.edinburghcharityfashionshow.org">ECFS 2014</a></p>
+            <h2>It's coming.</h2>
+            <h3><a href="http://2013.edinburghcharityfashionshow.org">ECFS 2014</a></h3>
+            <small>Enquires to <a href="mailto:info@edinburghcharityfashionshow.org">info@edinburghcharityfashionshow.org</a></small>
         </div>
     </div>
