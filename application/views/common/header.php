b1a4d596daf3477d3c576ed11c3e3fae8ba513c9
Tom Macmichael <tommacmichael@me.com>
2014-06-30 19:56:53 +0100

    made video background only work on web

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -25,16 +25,21 @@
     <title><?= $title ?></title>
 </head>
 <body>
+<?php
+    $this->load->library('mobile_detect');
+    $detect = new Mobile_Detect();
+    if (!($detect->isMobile() || $detect->isTablet())):
+?>
     <video autoplay loop poster="/assets/video/background.jpg" id="background" muted="true">
         <source src="/assets/video/background.mp4" type="video/mp4">
         <source src="/assets/video/background.webm" type="video/webm">
     </video>
+<? endif ?>
     <div class="container">
         <div class="row text-center">
             <p><img  width="190" src="/assets/img/ecfs-logo.png"></p>
             <h1>ECFS 2015</h1>
             <h2>It's coming.</h2>
-            <h3><a href="http://2013.edinburghcharityfashionshow.org">ECFS 2014</a></h3>
             <small>Enquires to <a href="mailto:info@edinburghcharityfashionshow.org">info@edinburghcharityfashionshow.org</a></small>
         </div>
     </div>
