b05c2f9dc69df9a1f6212800abd3770615c5a7fd
Tom Macmichael <t.macmichael@edinburghcharityfashionshow.org>
2014-10-07 12:11:20 +0100

    Designers done

--- application/views/design.php
+++ application/views/design.php
@@ -1,14 +1,35 @@
 <section id="design" class="section" data-speed="6" data-type="background" data-offsetY="250">
-    <div class="logo" data-type="sprite" data-offsetY="1250" data-Xposition="25%" data-speed="2"></div>
     <article>
         <div class="col-lg-12 content-row-3">
             <h1>Design</h1>
         </div>
-        <div class="col-lg-12">
-            <div class="col-lg-3">HEY</div>
-            <div class="col-lg-3">HEY</div>
-            <div class="col-lg-3">HEY</div>
-            <div class="col-lg-3">HEY</div>
+        <div class="col-sm-12 row ">
+            <div class="col-sm-3">
+                <img class="img-circle img-responsive" src="/assets/img/designers/east.jpg">
+            </div>
+            <div class="col-sm-3  content-row-3">
+                <p>This designer os great. Bla bla bla</p>
+            </div>
+            <div class="col-sm-3">
+                <img class="img-circle img-responsive" src="/assets/img/designers/east.jpg">
+            </div>
+            <div class="col-sm-3  content-row-3">
+                <p>This designer os great. Bla bla bla</p>
+            </div>
+        </div>
+        <div class="col-sm-12 row">
+            <div class="col-sm-3">
+                <img class="img-circle img-responsive" src="/assets/img/designers/east.jpg">
+            </div>
+            <div class="col-sm-3  content-row-3">
+                <p>This designer os great. Bla bla bla</p>
+            </div>
+            <div class="col-sm-3">
+                <img class="img-circle img-responsive" src="/assets/img/designers/east.jpg">
+            </div>
+            <div class="col-sm-3  content-row-3">
+                <p>This designer os great. Bla bla bla</p>
+            </div>
         </div>
     </article>
 </section>
