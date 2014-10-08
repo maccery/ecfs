78096ea9c85ddc35114b851962364b031f11ec67
Tom Macmichael <t.macmichael@edinburghcharityfashionshow.org>
2014-10-08 10:09:09 +0100

    last commit with sprite

--- application/views/splash.php
+++ application/views/splash.php
@@ -11,3 +11,23 @@
 <div id="splash" class="row text-center vertical-center">
     <p><a href="/home"><img width="400" border="0" src="/assets/img/ecfs-logo.png"></a></p>
 </div>
+
+<div id="coverflow">
+    <!-- these children will be part of the coverflow -->
+    <div class="something_fancy"></div>
+    <img src=".." />
+    <picture><source .. /></picture>
+</div>
+
+<!-- put this in your html header -->
+<link rel="stylesheet" type="text/css" href="coverflow.css" />
+<script src="jquery.min.js"></script>
+
+<!-- include coverflow after all dependencies-->
+<script src="coverflow.min.js"></script>
+<script>
+    $(function() {
+        // and kick off
+        $('#coverflow').coverflow();
+    });
+</script>
