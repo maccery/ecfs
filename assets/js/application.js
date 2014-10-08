4f11204e6718bb4c09fea3b15756f453d42b6ded
Tom Macmichael <t.macmichael@edinburghcharityfashionshow.org>
2014-10-08 12:25:19 +0100

    Frame animations sorted

--- assets/js/application.js
+++ assets/js/application.js
@@ -10,3 +10,30 @@ $('a[href^="#"]').on('click', function (e) {
         window.location.hash = target;
     });
 });
+
+var Ui = {
+    'sortFrame': function()
+    {
+        var offset = $(document).scrollTop();
+        if (offset >= 950)
+        {
+            $('.frame').fadeIn('slow', function() {
+            });
+        }
+        else
+        {
+            $('.frame').fadeOut('slow', function() {
+            });
+        }
+    }
+};
+
+/* When you scroll through the page, sort the frame out */
+$(window).scroll(function(){
+    Ui.sortFrame();
+});
+
+$(document).onload(function() {
+   Ui.sortFrame();
+});
+
