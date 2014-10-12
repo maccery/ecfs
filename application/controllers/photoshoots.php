2bbac43ddd91190a55389585f283287bd6e415b3
Tom Macmichael <t.macmichael@edinburghcharityfashionshow.org>
2014-10-12 22:51:33 +0100

    Photoshoots done

--- application/controllers/photoshoots.php
+++ application/controllers/photoshoots.php
@@ -1,7 +1,17 @@
 <?php
     class Photoshoots extends CI_Controller {
 
-        public function index($album_id = NULL)
+        public function index()
+        {
+            $this->photos();
+        }
+
+        public function view($album_id = NULL)
+        {
+            $this->photos($album_id);
+        }
+
+        private function photos($album_id = NULL)
         {
             $data['title'] = 'ECFS 2015 - Edinburgh Charity Fashion Show';
             $data['description'] = 'Edinburgh Charity Fashion Show (ECFS) 2015 - coming soon.';
@@ -14,19 +24,22 @@
             {
                 $album_id = $albums[0]->id;
             }
-            print_r($this->facebook->getAlbum($album_id));
+            // print_r($this->facebook->getAlbum($album_id));
+            $photos = $this->facebook->getAlbum($album_id);
             foreach ($photos as $photo)
             {
+                $photo = $photo->images[0];
                 $sources[] = $photo->source;
             }
 
-            $data['photos'] = $this->load->view('welcome', $sources, TRUE);
+            $data['sources'] = $sources;
+            $data['photos'] = $this->load->view('photoshoots/album', $data, TRUE);
 
             $data['albums'] = $albums;
 
-            //$this->load->view('common/header', $data);
-            ///$this->load->view('photoshoots/index', $data);
-            //$this->load->view('common/footer');
+            $this->load->view('common/header', $data);
+            $this->load->view('photoshoots/index', $data);
+            $this->load->view('common/footer');
         }
 
     }
--- application/views/sponsors.php
+++ application/views/sponsors.php
@@ -4,6 +4,15 @@
             <h1>Sponsors</h1>
         </div>
         <div class="row">
+            <p>
+                We would like to invite you sponsor the Edinburgh Charity Fashion Show 2015 supporting Scottish Mental
+                Health and Elephant Family. In exchange, we would happily advertise your business across all of our
+                social media outlets including Facebook, our official website and blog. Your company logo would be
+                featured on all official ECFS 2015 literature leading up to and on the night of the show. We would be
+                delighted to discuss this opportunity with you further, and we would appreciate the chance to present to
+                you the multitude of ways Edinburgh Charity Fashion Show 2015 could showcase your company to tens of
+                thousands of students.
+            </p>
             <p>sponsors@edinburghcharityfashionshow.org</p>
         </div>
     </article>
