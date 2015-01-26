4093c9d5aa184e04fd1050697ea7d18d4155dfd2
Tom Macmichael <t.macmichael@edinburghcharityfashionshow.org>
2015-01-26 11:41:31 +0000

    tickets and sponsors added

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -1,11 +1,11 @@
 
     </div>
-    <div class="text-center">
-        <a href="http://facebook.com/edifashionshow" target="_blank">Tickets</a>
-    </div>
     <hr>
     <footer role="navigation" style="width: 100%; color: black; height: 30px;">
         <div class="text-center">
+            <ul class="list list-inline  list-unstyled">
+                <li><a href="http://www.usherhall.co.uk/en-GB/shows/edinburgh%20charity%20fashion%20show%202015/info" target="_blank"><b>Tickets</b></a></li>
+            </ul>
             <ul class="list list-inline  list-unstyled">
                 <li><a href="http://facebook.com/edifashionshow" target="_blank">Facebook</a></li>
                 <li><a href="http://instagram.com/edifashionshow" target="_blank">Instagram</a></li>
--- application/views/sponsors.php
+++ application/views/sponsors.php
@@ -7,6 +7,12 @@
             <h1>Sponsors</h1>
         </div>
         <div class="row">
+            <div class="col-sm-4" style="padding: 4px;">
+                <a href="http://thisisourvision.com/" target="_blank"><img class="img-responsive" src="/assets/img/sponsors/nightvision.jpg" border="0"></a>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
             <p>
                 We would like to invite you to sponsor the Edinburgh Charity Fashion Show 2015 supporting Scottish Mental
                 Health and Elephant Family. In exchange, we would happily advertise your business across all of our
