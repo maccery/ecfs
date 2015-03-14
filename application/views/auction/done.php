9b6c590e5d6acf7f9afa296f14901aa37a502e90
Tom Macmichael <t.macmichael@edinburghcharityfashionshow.org>
2015-03-14 18:58:42 +0000

    Closed auction

---
+++ application/views/auction/done.php
@@ -0,0 +1,13 @@
+<section>
+    <div class="spacer">
+    </div>
+    <div class="row">
+        <ol class="breadcrumb">
+            <li><a href="/auction">Auction</a></li>
+        </ol>
+
+        <h2>The auction is now closed!</h2>
+
+        <p>Good luck to all those who bid!</p>
+    </div>
+</section>
